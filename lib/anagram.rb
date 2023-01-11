46a6c47a37c24b7816a8acd9c90be123161828b6
Seunggyu <seunggyul14@icloud.com>
2023-01-10 18:49:20 -0800

    commit tree be3821b5dce9b3543a5aecf045b287ad2db84d57

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.select {|x| x.split("").sort == @word.split("").sort}
+    end
+end
